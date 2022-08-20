efcd10e65b3fc7843ff81457ddba7982d538466a
Ramire7z <ramire7z.contato@gmail.com>
2022-08-20 09:47:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 print("Brasil", "ganhou", 5, "títulos mundiais", sep="-")
 
 pais = "Italia"
+quantidade = 4
 
 print(pais, "ganhou", quantidade, "títulos mundiais")
 
+print("Date {:02d}/{:02d}".format(8,20))
 
 
-user = input("Tell who is using the system!")
+user = input("Who is using the system? ")
 
 if(user == "Sandra"):
     print("Welcome, Sandra!")
