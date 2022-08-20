ff6dcb058c4915063699938d8139d729afa40c17
Ramire7z <ramire7z.contato@gmail.com>
2022-08-20 10:09:27 -0300

    Update adivinhacao.py

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -9,25 +9,29 @@ for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou ", chute_str)
-    chute = int(chute_str)
+    try:
+        chute = int(chute_str)
+    except:
+        print("Você precisa digitar um número entre 1 e 100, não utilize letras!")
+        continue
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
     if chute < 1 or chute > 100:
         print("Você deve digitar um número entre 1 e 100!")
         continue
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    if acertou:
+        print("Parabéns! Você acertou.")
+        break
+    else:
+        if maior:
+            print("Você errou! O seu chute foi maior do que o número secreto")
+        elif menor:
+            print("Você errou! O seu chute foi menor do que o número secreto")
 
-if acertou:
-    print("Parabéns! Você acertou.")
-    break
-else:
-    if maior:
-        print("Você errou! O seu chute foi maior do que o número secreto")
-    elif menor:
-        print("Você errou! O seu chute foi menor do que o número secreto")
-
-rodada = rodada + 1
+    rodada = rodada + 1
 
 print("Fim do jogo")
