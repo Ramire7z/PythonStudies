7c0a7f36e49c94a54565226e8571dcdb3c8fbd8d
Ramire7z <ramire7z.contato@gmail.com>
2022-08-20 09:23:36 -0300

    Adivinhacao

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 print("Brasil", "ganhou", 5, "títulos mundiais", sep="-")
 
+pais = "Italia"
+
+print(pais, "ganhou", quantidade, "títulos mundiais")
+
+
+
+user = input("Tell who is using the system!")
+
+if(user == "Sandra"):
+    print("Welcome, Sandra!")
+elif(user == "Rodolfo"):
+    print("Welcome, Rodolfo!")
+elif(user == "Jaspion"):
+    print("Welcome, Jaspion!")
+else:
+    print("User undefined!")
